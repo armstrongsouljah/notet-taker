b2640021f9570911aa1b93c5fd912c52be7b1169
Armstrong <armstrongsouljah@gmail.com>
2020-05-29 08:27:02 +0300

    Add CRUD controllers

---
+++ src/notes/dto/createNotesDto.ts
@@ -0,0 +1,5 @@
+export class CreateNoteDto {
+    readonly name: string;
+    readonly description: string;
+    readonly idstring: string;
+}
--- src/notes/notes.controller.ts
+++ src/notes/notes.controller.ts
@@ -1,4 +1,49 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
+import {CreateNoteDto} from './dto/createNotesDto';
+
+const notes: Array<any> = [];
 
 @Controller('notes')
-export class NotesController {}
+export class NotesController {
+   @Get()
+   findAll() {
+       return {
+           message: 'Listing notes',
+           notes: notes
+       }
+   }
+
+   @Get(':idstring')
+   findOne(@Param() param): object {
+        return {
+            message: `Found ${param.idstring}`
+        }
+   }
+
+   @Post('create')
+   create(@Body() noteCreateDto: CreateNoteDto) {
+       return {
+           message: 'Saving Note',
+           name: noteCreateDto.name,
+           description: noteCreateDto.description,
+           idstring: noteCreateDto.idstring
+       }
+   }
+
+   @Put(':idstring')
+   updateOne(@Param() params, @Body() updateDto: CreateNoteDto) {
+        return {
+            message: `Updating note ${params.idstring}`,
+            name: updateDto.name,
+            description: updateDto.description,
+            idstring: updateDto.idstring
+        }
+   }
+
+   @Delete(':idstring')
+   findAndDelete(@Param() params) {
+       return {
+           message: `Successfully deleted ${params.idstring}`
+       }
+   }
+}
